09de260bc000276e133074211b3870678a9335ae
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-07-13 19:17:20 -0400

    Fix: settings

--- tramos_api/settings.py
+++ tramos_api/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para collectstatic
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # Tus archivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Tus archivos estáticos
 
 
 # Default primary key field type
