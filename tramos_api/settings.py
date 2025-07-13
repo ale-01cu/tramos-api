14787025e6a222ae9483db9a14682b97375c861c
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-07-13 19:09:36 -0400

    Fix: settings

--- tramos_api/settings.py
+++ tramos_api/settings.py
@@ -164,8 +164,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Tus archivos estáticos
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
