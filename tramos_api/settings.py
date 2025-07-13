005a461dcd910b8a0b27bfe619fc4640de774659
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-07-13 19:16:23 -0400

    Fix: settings

--- tramos_api/settings.py
+++ tramos_api/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para collectstatic
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # Tus archivos estáticos
 
